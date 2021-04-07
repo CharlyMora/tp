6117dc0e44f01e6f79887569a8d87274c83c93d6
CharlyMora <morandres.carlos@gmail.com>
2021-04-07 12:11:04 -0500

    finalizado

--- scripts/dist/map.dev.js
+++ scripts/dist/map.dev.js
@@ -1,12 +1,49 @@
 "use strict";
 
+var isACardActive = true;
+
 function cardtoggle(cardName) {
   var id = cardName + "card";
   var x = document.getElementById(id);
 
-  if (x.style.display === "none") {
-    x.style.display = "flex";
+  if (isACardActive) {
+    hideCards();
   } else {
-    x.style.display = "none";
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+      isACardActive = true;
+    } else {
+      x.style.display = "none";
+      isACardActive = false;
+    }
   }
+}
+
+function hideCards() {
+  var y = document.getElementsByClassName('card');
+  var _iteratorNormalCompletion = true;
+  var _didIteratorError = false;
+  var _iteratorError = undefined;
+
+  try {
+    for (var _iterator = y[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
+      var item = _step.value;
+      item.style.display = "none";
+    }
+  } catch (err) {
+    _didIteratorError = true;
+    _iteratorError = err;
+  } finally {
+    try {
+      if (!_iteratorNormalCompletion && _iterator["return"] != null) {
+        _iterator["return"]();
+      }
+    } finally {
+      if (_didIteratorError) {
+        throw _iteratorError;
+      }
+    }
+  }
+
+  isACardActive = false;
 }
--- scripts/map.js
+++ scripts/map.js
@@ -1,9 +1,32 @@
+let isACardActive =true;
+
 function cardtoggle(cardName) {
     let id = cardName+"card"
     let x = document.getElementById(id)
-    if (x.style.display === "none") {
-    x.style.display = "flex";
-    } else {
-    x.style.display = "none";
+
+    if(isACardActive) {
+        hideCards()
+    }else{
+        if (x.style.display === "none") {
+            x.style.display = "flex";
+            isACardActive =true
+        } else {
+            x.style.display = "none";
+            isACardActive=false
+        }
+    
     }
+
+    
+    
+}
+
+function hideCards() {
+    let y = document.getElementsByClassName('card')
+    
+    for (let item of y) {
+        item.style.display = "none"
+    }
+
+    isACardActive=false
 }
